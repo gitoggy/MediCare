be33069be3e41603493a04401a91cc791e5ecf67
Kopal <kopaljain03@gmail.com>
2021-10-09 16:40:42 +0530

    new files added

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,29 @@
+//importing npm dependencies
 const express = require("express");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
+//importing other dependencies
+const doctorRoute=require('./routes/doctor')
+const patientRoute=require('./routes/patient')
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Backend");
-});
+//declaring middleware usage
+app.use(express.urlencoded())
+app.use(express.json())
+app.use(session({
+    secret: 'blackcatnero',
+    resave: false,
+    saveUninitialized: true
+  }))
+ 
+app.use('/doctor',doctorRoute)
+app.use('/patient',patientRoute)
 
+
+//starting app on port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
