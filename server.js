19d39a8382969b69063dd93d466d6a003eff490c
Kopal <kopaljain03@gmail.com>
2021-10-12 12:46:43 +0530

    react removed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const session = require('express-session')
 const dotenv = require("dotenv");
 const upload=require('express-fileupload')
-
+const path=require('path')
 
 dotenv.config();
 
